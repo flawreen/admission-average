92fee5b0ac80c1c2bf9946a13aab231df8dbdf9f
Florin <83332450+flawreen@users.noreply.github.com>
2022-02-24 21:35:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+class Materie:
+    def __init__(self, nume):
+        self.nota = None
+        self.nume = nume
+
+    def setNota(self):
+        self.nota = float(input("Nota " + self.nume + ": "))
+
+    def getNota(self):
+        return 0.5 * self.nota
+
+
+def calc():
+    return 0.2 * bac.getNota() + 0.8 * (mate.getNota() + info.getNota())
+
+
+if __name__ == '__main__':
+    mate = Materie("Matematica")
+    info = Materie("Informatica")
+    bac = Materie("Bacalaureat")
+
+    mate.setNota()
+    info.setNota()
+    bac.setNota()
+
+    print("{:.2f}".format(calc()))
